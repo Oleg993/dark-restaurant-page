3aeb2d01e68fb724f5cc331d569e54a9b1ca0486
Aleh Shulhan <121169496+Oleg993@users.noreply.github.com>
2023-03-25 15:40:56 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -20,12 +20,55 @@ document.querySelector('.slider-back').addEventListener('click', function () {
 
 /*Ranking*/
 
-const rankingItemsList = document.querySelectorAll('.ranking-item');
-const rankingItemsArray = Array.prototype.slice.call(rankingItemsList);
+window.onload = function () {
+  const rankingItemsList = document.querySelectorAll('.ranking-item');
+  const rankingItemsArray = Array.prototype.slice.call(rankingItemsList);
 
-rankingItemsArray.forEach((item) =>
-  item.addEventListener('click', () => {
-    const { itemValue } = item.dataset;
-    item.parentNode.dataset.totalValue = itemValue;
-  })
-);
+  rankingItemsArray.forEach((item) =>
+    item.addEventListener('click', () => {
+      const { itemValue } = item.dataset;
+      item.parentNode.dataset.totalValue = itemValue;
+    })
+  );
+};
+
+/*Section 3*/
+
+let listSection3 = document.querySelector('.third-page-items');
+let key;
+
+async function fillList() {
+  let response = await fetch('./section3.json');
+  let content = await response.json();
+
+  let template = '';
+
+  const contentArray = Array.prototype.slice.call(content);
+  contentArray.forEach((element) => {
+    template += `
+<div class="item-third-page">
+    <div class="item-third-page-img">
+      <img src="${element.img}" alt="" />
+    </div>
+    <div class="item-third-page-text">
+      <h3>${element.title}</h3>
+      <p class="third-page-p-1">${element.description1}</p>
+      <p class="third-page-p-2">${element.description2}</p>
+      <div class="item-third-footer">
+        <div class="item-third-ranking" data-total-value="0">
+          <div class="ranking-item" data-item-value="5">${element.star}</div>
+          <div class="ranking-item" data-item-value="4">${element.star}</div>
+          <div class="ranking-item" data-item-value="3">${element.star}</div>
+          <div class="ranking-item" data-item-value="2">${element.star}</div>
+          <div class="ranking-item" data-item-value="1">${element.star}</div>
+        </div>
+        <a class="item-third-btn" href="#!">${element.btn}</a>
+      </div>
+    </div>
+  </div>
+`;
+  });
+
+  return listSection3.insertAdjacentHTML('afterbegin', template);
+}
+fillList();
